8c613283992a09eb56e7511f73412dd0ec2c4ee0
lab_services_student <dlungwanesilo15@gmail.com>
2025-06-19 08:51:41 +0200

    first commit

---
+++ app/src/main/java/vcmsa/ci/playlistmanagerapp/MainActivity.kt
@@ -0,0 +1,48 @@
+package vcmsa.ci.playlistmanagerapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.system.exitProcess
+
+class MainActivity : AppCompatActivity() {
+
+    private val SongTitle = mutableListOf<String>()
+    private val ArtistsName= mutableListOf<String>()
+    private val  Rating= mutableListOf<Int>()
+    private val  Comments= mutableListOf<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val SongTitleEditText = findViewById<EditText>(R.id.SongTitleEditText)
+        val ArtistNameEditText = findViewById<EditText>(R.id.ArtistNameEditText)
+        val RatingEditText = findViewById<EditText>(R.id.RatingEditText)
+        val CommentsEditText = findViewById<EditText>(R.id.CommentsEditText)
+        val AddBtn = findViewById<Button>(R.id.AddBtn)
+        val playlistBtn = findViewById<Button>(R.id.PlaylistBtn)
+        val exitBtn = findViewById<Button>(R.id.ExitBtn)
+
+
+        AddBtn.setOnClickListener {
+            showListOnDialog()
+        }
+        playlistBtn.setOnClickListener {
+            // val intent
+        }
+        exitBtn.setOnClickListener {
+            finishAffinity()
+            exitProcess(1)
+        }
+
+
+        }
+    private fun showListOnDialog(){
+
+    }
+    }
